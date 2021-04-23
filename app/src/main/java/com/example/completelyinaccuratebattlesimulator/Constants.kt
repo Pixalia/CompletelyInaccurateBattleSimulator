658d7458fea93584b69933c31d53d4400ef2236a
Pixalia <davidcalsp@gmail.com>
2021-04-23 11:21:33 -0700

    got Backendless working

---
+++ app/src/main/java/com/example/completelyinaccuratebattlesimulator/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.completelyinaccuratebattlesimulator
+
+class Constants {
+
+    companion object {
+        val API_KEY = "5EC80385-99FC-4BC4-937C-431873C932BB"
+        val API_ID = "E4945278-F83D-4185-A76B-4CF3057EA6DA"
+    }
+
+}
--- app/src/main/java/com/example/completelyinaccuratebattlesimulator/LoginActivity.kt
+++ app/src/main/java/com/example/completelyinaccuratebattlesimulator/LoginActivity.kt
@@ -1,15 +1,59 @@
 package com.example.completelyinaccuratebattlesimulator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.SyncStateContract
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.backendless.Backendless
+import com.backendless.BackendlessUser
+import com.backendless.async.callback.AsyncCallback
+import com.backendless.exceptions.BackendlessFault
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        val EXTRA_USERNAME = "username"
+        val EXTRA_PASSWORD = "password"
+        val REQUEST_LOGIN_INFO = "login"
+        val TAG = "LOGINACTIVITY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        Backendless.initApp(this, Constants.API_ID, Constants.API_KEY)
+
         button_login_login.setOnClickListener {
+
+            val username = editText_login_username.text.toString()
+            val password = editText_login_password.text.toString()
+
+            Backendless.UserService.login(username, password, object: AsyncCallback<BackendlessUser> {
+
+                override fun handleFault(fault: BackendlessFault?) {
+
+                    Toast.makeText(this@LoginActivity, "Something went wrong, check the logs.", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "handleFault: " + fault?.message)
+
+                }
+
+                override fun handleResponse(response: BackendlessUser?) {
+
+                    Toast.makeText(this@LoginActivity, "${response?.userId} has logged in.", Toast.LENGTH_SHORT).show()
+                    //startActivity()
+                    finish()
+
+                }
+
+            })
+
+        }
+
+        button_login_createAccount.setOnClickListener {
 
         }
     }
