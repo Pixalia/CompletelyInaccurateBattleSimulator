fe746313ceae2f702420724117f5e4138fd52ac1
Pixalia <davidcalsp@gmail.com>
2021-05-05 10:33:42 -0700

    sounds player basic added

--- app/src/main/java/com/example/completelyinaccuratebattlesimulator/HomePage.kt
+++ app/src/main/java/com/example/completelyinaccuratebattlesimulator/HomePage.kt
@@ -3,12 +3,18 @@ package com.example.completelyinaccuratebattlesimulator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.media.AudioManager
+import android.media.MediaPlayer
 
 class HomePage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val kyantaPlayer = MediaPlayer.create(this, R.raw.kyanta)
+
+        kyantaPlayer.start()
     }
 
 }
