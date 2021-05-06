9266043b0d8d0ec7ec6c60a8946ad03a50b93efc
Pixalia <davidcalsp@gmail.com>
2021-05-05 17:49:45 -0700

    fixed the music player

--- app/src/main/java/com/example/completelyinaccuratebattlesimulator/HomePage.kt
+++ app/src/main/java/com/example/completelyinaccuratebattlesimulator/HomePage.kt
@@ -5,16 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.view.View
 
 class HomePage : AppCompatActivity() {
+
+    var kyantaPlayer : MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val kyantaPlayer = MediaPlayer.create(this, R.raw.kyanta)
+        kyantaPlayer = MediaPlayer.create(this, R.raw.kyanta)
+        kyantaPlayer!!.isLooping = true
+        kyantaPlayer!!.start()
+    }
 
-        kyantaPlayer.start()
+    override fun onPause(){
+        super.onPause()
+        kyantaPlayer!!.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        kyantaPlayer!!.start()
     }
 
 }
