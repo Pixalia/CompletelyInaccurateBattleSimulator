010a5c4fa278b0c31a3a24d0d72fabb534fb54a8
Pixalia <davidcalsp@gmail.com>
2021-05-10 11:59:33 -0700

    Added code to make character creation work, but still need to set the createdCharacter to true somehow...

--- app/src/main/java/com/example/completelyinaccuratebattlesimulator/CreationActivity.kt
+++ app/src/main/java/com/example/completelyinaccuratebattlesimulator/CreationActivity.kt
@@ -1,16 +1,16 @@
 package com.example.completelyinaccuratebattlesimulator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.backendless.Backendless
-import com.backendless.Backendless.Data
 import com.backendless.BackendlessUser
+import com.backendless.UserService
 import com.backendless.async.callback.AsyncCallback
 import com.backendless.exceptions.BackendlessFault
 import kotlinx.android.synthetic.main.activity_creation.*
-import kotlinx.android.synthetic.main.activity_registration.*
 
 class CreationActivity : AppCompatActivity() {
 
@@ -27,6 +27,9 @@ class CreationActivity : AppCompatActivity() {
         button_creation_backToMain.setOnClickListener {
             if (user.getProperty("createdCharacter") == false){
                 Toast.makeText(this, "Error: No character data found!", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                creationFinished()
             }
         }
 
@@ -86,7 +89,28 @@ class CreationActivity : AppCompatActivity() {
 
             override fun handleResponse(response: Character?) {
                 Toast.makeText(this@CreationActivity, "Character created!", Toast.LENGTH_SHORT).show()
-                user.setProperty("createdCharacter", true)
+                creationFinished()
+            }
+
+        })
+    }
+
+    private fun creationFinished(){
+        user.setProperty("createdCharacter", true)
+        Backendless.UserService.update(user, object: AsyncCallback<BackendlessUser>{
+            override fun handleFault(fault: BackendlessFault?) {
+
+                Log.d(TAG, "handleFault : ${fault?.detail}")
+
+            }
+
+            override fun handleResponse(response: BackendlessUser?) {
+
+                val creationIntent = Intent(this@CreationActivity, HomePage::class.java)
+                startActivity(creationIntent)
+                //to close the login screen so it's not there when user clicks bacc
+                finish()
+
             }
 
         })
